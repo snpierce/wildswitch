129bfc3c976c04013ef10a10ac4f49b8cf2eed68
snpierce <90164737+snpierce@users.noreply.github.com>
2021-11-26 17:50:47 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,167 @@
+mport os
+import datetime
+
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///wildswitch.db")
+
+with open("People.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        db.execute("INSERT INTO people VALUES ?, ?, ?, ?, ?, ?, ?, ?, ?, ?", row["playerID"], row["nameFirst"], row["nameLast"], row["nameGiven"],
+        row["weight"], row["height"], row["bats"], row["throws"], row["debut"], row["finalGame"])
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # when register button is clicked
+    if request.method == "POST":
+
+        # checks that all forms have valid input
+        if not request.form.get("username"):
+            return apology("Please enter username.")
+        elif not request.form.get("password"):
+            return apology("Please enter password.")
+        elif not request.form.get("confirmation"):
+            return apology("Please enter confirmation.")
+
+        # checks if username is already in users table, stores 1 if found, 0 if not
+        x = db.execute("SELECT COUNT(*) AS 'count' FROM users WHERE username = ?", request.form.get("username"))
+
+        # if username already in users table, return apology message 
+        if 0 != x[0]["count"]:
+            return apology("Username Taken")
+        
+        # checks if password and confirmation match, return apology if don't
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("Passwords do not match.")
+
+        # hash password
+        hash = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
+
+        # if passes all the checks, add username+hash to users table
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), hash)
+        
+        # return users info for now registered user by searching for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        
+        # log user into session by id
+        session["user_id"] = rows[0]["id"]
+
+        # redirect user to home page
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/")
+@login_required
+def index():
+       return
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+        return
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+        return
+
+@app.route("/history")
+@login_required
+def history():
+       return
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
 
