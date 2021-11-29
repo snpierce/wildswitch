05e548fcc2e7af468c6a409882c5f30783777b63
snpierce <90164737+snpierce@users.noreply.github.com>
2021-11-28 20:52:41 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,17 +1,15 @@
 import os, csv
 import datetime
-import sqlalchemy as db
+import sqlite3
 
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup
+from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
@@ -25,15 +23,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# set up SQL database
+con = sqlite3.connect("wildswitch.sqlite")
+cur = con.cursor()
 
-
-# database engine object from SQLAlchemy that manages connections to the database
-engine = db.create_engine(os.getenv("DATABASE_URL"))
-
-# create a 'scoped session' that ensures different users' interactions with the
-# database are kept separate
-db = scoped_session(sessionmaker(bind=engine))
 
 @app.after_request
 def after_request(response):
@@ -42,52 +34,6 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user"""
-
-    # when register button is clicked
-    if request.method == "POST":
-
-        # checks that all forms have valid input
-        if not request.form.get("username"):
-            return apology("Please enter username.")
-        elif not request.form.get("password"):
-            return apology("Please enter password.")
-        elif not request.form.get("confirmation"):
-            return apology("Please enter confirmation.")
-
-        # checks if username is already in users table, stores 1 if found, 0 if not
-        x = db.execute("SELECT COUNT(*) AS 'count' FROM users WHERE username = ?", request.form.get("username"))
-
-        # if username already in users table, return apology message 
-        if 0 != x[0]["count"]:
-            return apology("Username Taken")
-        
-        # checks if password and confirmation match, return apology if don't
-        if request.form.get("password") != request.form.get("confirmation"):
-            return apology("Passwords do not match.")
-
-        # hash password
-        hash = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
-
-        # if passes all the checks, add username+hash to users table
-        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), hash)
-        
-        # return users info for now registered user by searching for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-        
-        # log user into session by id
-        session["user_id"] = rows[0]["id"]
-
-        # redirect user to home page
-        return redirect("/")
-
-    else:
-        return render_template("register.html")
-
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -109,7 +55,7 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = cur.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
@@ -136,35 +82,49 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-@app.route("/")
-@login_required
-def index():
-       return render_template("index.html")
 
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-        return
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
 
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-        return
+    # when register button is clicked
+    if request.method == "POST":
 
-@app.route("/history")
-@login_required
-def history():
-       return
+        # checks that all forms have valid input
+        if not request.form.get("username"):
+            return apology("Please enter username.")
+        elif not request.form.get("password"):
+            return apology("Please enter password.")
+        elif not request.form.get("confirmation"):
+            return apology("Please enter confirmation.")
 
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return apology(e.name, e.code)
+        # checks if username is already in users table, stores 1 if found, 0 if not
+        x = cur.execute("SELECT COUNT(*) AS 'count' FROM Users WHERE username = ?", request.form.get("username"))
 
+        # if username already in users table, return apology message 
+        if 0 != x[0]["count"]:
+            return apology("Username Taken")
+        
+        # checks if password and confirmation match, return apology if don't
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("Passwords do not match.")
 
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
+        # hash password
+        hash = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
 
+        # if passes all the checks, add username+hash to users table
+        cur.execute("INSERT INTO Users (username, hash) VALUES (?, ?)", request.form.get("username"), hash)
+        
+        # return users info for now registered user by searching for username
+        rows = cur.execute("SELECT * FROM Users WHERE username = ?", request.form.get("username"))
+        
+        # log user into session by id
+        session["user_id"] = rows[0]["id"]
 
+        # redirect user to home page
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
+
+con.close()
