27970ec0374bd85dbfa617b38623a2de31fee653
snpierce <n.pierce@yale.edu>
2021-12-08 14:21:56 -0500

    progress

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def buy():
          elif status == 1:
              return apology("Already purchased!")
          else:
-             cur.execute("INSERT INTO Cards (username, playerID, cardValue, status, fullName) VALUES (?, ?, ?, ?, ?)", 
+             cur.execute("INSERT INTO Cards (username, playerID, cardValue, status, position) VALUES (?, ?, ?, ?, ?)", 
              (session["user_id"], players[player][0], value, '0', position))
              cur.execute("UPDATE Users SET cash = cash - ? WHERE username = ?", (value, session["user_id"],))
              cur.execute("UPDATE People SET status = '1' WHERE playerID = ?", (players[player][0],))
@@ -79,6 +79,27 @@ def buy():
 
         return render_template("buy.html", players=players)
 
+app.route("/mycards", methods=["GET", "POST"])
+@login_required
+def mycards():
+
+    if request.method == "POST":
+         
+         cur.execute("UPDATE Users SET cash = ? WHERE username = ?", )
+    else:
+        cur.execute("SELECT * FROM Batting WHERE playerID IN (SELECT playerID FROM Cards WHERE username = ?)", (session["user_id"],))
+        batters = list(cur.fetchall())
+
+        cur.execute("SELECT * FROM Pitching WHERE playerID IN (SELECT playerID FROM Cards WHERE username = ?)", (session["user_id"],))
+        pitchers = list(cur.fetchall())
+
+        cur.execute("SELECT cash FROM Users WHERE username = ?", (session["user_id"],))
+        cash = int(cur.fetchone()[0])
+
+        cur.execute("SELECT COUNT(*) FROM Cards WHERE username = ?", (session["user_id"],))
+        cardCount = int(cur.fetchone()[0])
+        
+        return render_template("mycards.html", batters=batters, pitchers=pitchers, cash=cash, cardCount=cardCount)
 
 @app.route("/")
 @login_required
